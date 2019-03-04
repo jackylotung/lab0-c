ce5264c22ba1bd7dd527bb5447e72181ae51702d
Parallels <jackylotung@gmail.com>
2019-03-04 19:00:19 +0800

    Insert at the head of the queue

--- queue.c
+++ queue.c
@@ -66,14 +66,35 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (!q) {
+        return false;
+    }
     list_ele_t *newh;
     /* What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
-    newh->next = q->head;
-    q->head = newh;
-    return true;
+    if (!newh) {
+        return false;
+    }
+    newh->value = malloc(strlen(s) + 1);
+    if (!newh->value) {
+        free(newh);
+        return false;
+    }
+    strpcy(newh->value, s);
+    if (q->size == 0) {
+        q->tail = newh;
+        newh->next = NULL;
+        q->head = newh;
+        q->size++;
+        return true;
+    } else {
+        newh->next = q->head;
+        q->head = newh;
+        q->size++;
+        return true;
+    }
 }
 
 
