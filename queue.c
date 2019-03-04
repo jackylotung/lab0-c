d0cf4ecb8ea75b090b20b6cc7e3baac50bf20dc0
Parallels <jackylotung@gmail.com>
2019-03-04 19:31:15 +0800

    To find the size of the queue

--- queue.c
+++ queue.c
@@ -164,7 +164,10 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
-    return 0;
+    if (!q) {
+        return 0;
+    }
+    return q->size;
 }
 
 /*
